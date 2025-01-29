1e39060155d5db3142b78b7477d5e89fca1d696c
Ethaniel Amorim <ethamorim@gmail.com>
2025-01-29 20:51:06 -0300

    feat: remove markdown lib

--- database2prompt/markdown/generator.py
+++ database2prompt/markdown/generator.py
@@ -1,7 +1,6 @@
-import markdown
-
 def generate_markdown(content: str, output_file: str):
     """Gera um arquivo Markdown a partir de uma string."""
-    md_content = markdown.markdown(content)
+
+    md_content = ""
     with open(output_file, "w") as file:
         file.write(md_content)
