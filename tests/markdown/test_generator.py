cbe2863a0d52de42efa6e3946f34b02e1ceb8bf3
Ethaniel Amorim <ethamorim@gmail.com>
2025-01-29 21:20:49 -0300

    test: create database table of contents test

---
+++ tests/markdown/test_generator.py
@@ -0,0 +1,14 @@
+from database2prompt.markdown.generator import generate_markdown
+
+def test_should_generate_database_table_of_contents():
+    tables = ["table_a", "table_b", "table_c"]
+    markdown = generate_markdown(tables)
+
+    lines = markdown.splitlines()
+    assert len(lines) == 4
+    assert lines[0] == "# Table of contents"
+    assert lines[1] == "- table_a"
+    assert lines[2] == "- table_b"
+    assert lines[3] == "- table_c" 
+    
+    
