73c1c0c318b89e172dbafef903734df645487114
Ethaniel Amorim <ethamorim@gmail.com>
2025-02-04 18:38:37 -0300

    feat: add table creation sql to markdown

--- database2prompt/main.py
+++ database2prompt/main.py
@@ -2,15 +2,14 @@ from database2prompt.database import connection
 from database2prompt.markdown.generator import generate_markdown
 
 def main():
-    db = next(connection.get_db())
+    next(connection.get_db())
     print("Connected to the database!")
 
     tables = connection.list_tables()
     print("tables: ", tables)
 
-    output_file = "database-summary.md"
-
     generated_markdown = generate_markdown(tables)
+    output_file = "dist/output.md"
     with open(output_file, "w") as file:
         file.write(generated_markdown)
         
--- database2prompt/markdown/generator.py
+++ database2prompt/markdown/generator.py
@@ -1,8 +1,28 @@
+from database2prompt.database.connection import engine
+
+from sqlalchemy import MetaData, Table
+
 def generate_markdown(tables: list[str]):
     """Generates a markdown file given a string value."""
 
-    md_content = "# Table of contents\n"
+    md_content = "# Contents\n"
     for table in tables:
         md_content += f"- {table}\n"
+    md_content += "\n"
+
+    for table_name in tables:
+        metadata = MetaData()
+        table = Table(table_name, metadata, autoload_with=engine)
+
+        md_content += f"## Table: {table_name}\n\n"
+
+        full_qualified_name = f"{table.schema}.{table_name}" if table.schema != None else table_name
+        md_content += f"CREATE TABLE {full_qualified_name} (\n"
+
+        for (name, column) in table.columns.items():
+            md_content += f"    {name} {column.type}\n"
+        md_content += ");\n\n"
+
+
         
     return md_content
